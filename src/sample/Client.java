a686c609c5c15956cddfa560554c41cd3d8b5d5d
leherguargin <maciejleszczynski1997@gmail.com>
2021-06-11 16:10:35 +0200

    add adding clients

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -4,41 +4,69 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import static java.lang.System.nanoTime;
 
 public class Client {
-    private final ArrayList<File> files = new ArrayList<>();
     private final long time;
     private final String name;
-    private static final int MAX_FILES = 3;
+    private final long size0;
+    private final long size1;
+    private final long size2;
+    private final long id;
+    public boolean uploaded = false;
+    public long prior = 0;
+    public static final int MAX_FILES = 3;
+    private final int MAX_SIZE = 10000;
 
-    Client(String name) {
+    Client(String name, long id) {
+        this.id = id;
         this.time = nanoTime();
         this.name = name;
         Random random = new Random();
-        int filesCount = 1 + random.nextInt(MAX_FILES);
-        for (int i = 0; i < filesCount; i++) {
-            this.files.add(new File());
-        }
+        size0 = random.nextInt(MAX_SIZE);
+        size1 = random.nextInt(MAX_SIZE);
+        size2 = random.nextInt(MAX_SIZE);
+    }
 
+    public long getTime() {
+        return time;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize0() {
+        return size0;
+    }
+
+    public long getSize1() {
+        return size1;
+    }
+
+    public long getSize2() {
+        return size2;
     }
 
     public long getWaitingTime() {
         return nanoTime() - time;
     }
 
-    public ArrayList<File> getFiles() {
-        return files;
+    public long getSizes() {
+        return size0 + size1 + size2;
     }
+
 
     public VBox render() {
         VBox clientLayout = new VBox();
@@ -62,27 +90,27 @@ public class Client {
         group.getChildren().addAll(rect, clientName);
 
         int fileNumber = 1;
-        for (File file : files) {
-            VBox fileData = new VBox();
-            Label fileName = new Label("file" + fileNumber);
-            fileName.setAlignment(Pos.CENTER);
-            fileName.setContentDisplay(ContentDisplay.CENTER);
-            fileName.setLayoutX(0.0);
-            double Y1 = 17.0 * fileNumber++;
-            fileName.setLayoutY(Y1);
-            fileName.setPrefWidth(80.0);
-            fileName.setPrefHeight(17.0);
-
-            ProgressBar progressBar = new ProgressBar(file.getProgress());
-            progressBar.setLayoutX(5.0);
-            double Y2 = 17.0 * fileNumber++;
-            progressBar.setLayoutY(Y2);
-            progressBar.setPrefWidth(80.0);
-            progressBar.setPrefHeight(17.0);
-
-            fileData.getChildren().addAll(fileName, progressBar);
-            group.getChildren().add(fileData);
-        }
+//        for (File file : files) {
+//            VBox fileData = new VBox();
+//            Label fileName = new Label("file" + fileNumber);
+//            fileName.setAlignment(Pos.CENTER);
+//            fileName.setContentDisplay(ContentDisplay.CENTER);
+//            fileName.setLayoutX(10.0);
+//            double Y1 = 17.0 * fileNumber++;
+//            fileName.setLayoutY(Y1);
+//            fileName.setPrefWidth(80.0);
+//            fileName.setPrefHeight(17.0);
+//
+//            ProgressBar progressBar = new ProgressBar(file.getProgress());
+//            progressBar.setLayoutX(5.0);
+//            double Y2 = 17.0 * fileNumber++;
+//            progressBar.setLayoutY(Y2);
+//            progressBar.setPrefWidth(80.0);
+//            progressBar.setPrefHeight(17.0);
+//
+//            fileData.getChildren().addAll(fileName, progressBar);
+//            group.getChildren().add(fileData);
+//        }
 
         clientLayout.getChildren().add(group);
         return clientLayout;
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,56 +1,118 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private TableView tableView = new TableView();
+    private final ObservableList<Client> data = FXCollections.observableArrayList();
+    int clientsNumber = 0;
+    private long id = 1;
+    String[] uploading = {" - ", " - ", " - ", " - "};
+    Boolean[] available = {true, true, true, true};
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-//       Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         VBox mainLayout = new VBox();
         mainLayout.setPadding(new Insets(15.0, 12.0, 15.0, 12.0));
         mainLayout.setSpacing(10.0);
         mainLayout.setStyle("-fx-background-color: #7d8ea3;");
 
         HBox topLayout = new HBox();
+        Button addClient = new Button("Add client");
+        addClient.setOnAction((e) -> {
+            Client client = new Client("c" + ++clientsNumber, id++);
+            data.add(client);
+        });
         topLayout.setSpacing(10.0);
-        topLayout.getChildren().addAll(generateDisk("disk 1"), generateDisk("disk 2"), generateDisk("disk 3"), generateDisk("disk 4"));
+        topLayout.getChildren().addAll(
+                generateDisk("disk 0", uploading[0]),
+                generateDisk("disk 1", uploading[1]),
+                generateDisk("disk 2", uploading[2]),
+                generateDisk("disk 3", uploading[3]),
+                addClient
+        );
 
-        HBox bottomLayout = new HBox();
+        tableView.setEditable(true);
 
-        Client client1 = new Client("client 1");
+        TableColumn name = new TableColumn("Client name");
+        name.setCellValueFactory(new PropertyValueFactory<Client, String>("name"));
+        TableColumn id = new TableColumn("Client id");
+        id.setCellValueFactory(new PropertyValueFactory<Client, String>("id"));
+        TableColumn time = new TableColumn("time");
+        time.setCellValueFactory(new PropertyValueFactory<Client, String>("time"));
 
-        bottomLayout.getChildren().add(client1.render());
+        TableColumn files = new TableColumn("size");
+        for (int i = 0; i < Client.MAX_FILES; i++) {
+            TableColumn tc = new TableColumn("file " + i);
+            tc.setCellValueFactory(new PropertyValueFactory<Client, String>("size" + i));
+            files.getColumns().add(tc);
+        }
 
-        mainLayout.getChildren().addAll(topLayout, bottomLayout);
+        tableView.setItems(data);
+
+        tableView.getColumns().addAll(id, name, time, files);
+
+        mainLayout.getChildren().addAll(topLayout, tableView);
         Scene scene = new Scene(mainLayout, 800.0, 600.0);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Disk simulation");
         primaryStage.show();
+        Thread disksWorker = new Thread(() -> {
+            while (true) {
+                for (int j = 0; j < 4; j++) {
+                    if (available[j] && !data.isEmpty()) {
+                        //licz priorytet oraz uploaduj klienta z największym
+                        System.out.println("start dla dysku: " + j);
+                        long maxPrior = 0;
+                        Client clientWithMaxPrior = null;
+                        for (int i = 0; i < data.size(); i++) {
+                            if (data.get(i).uploaded)
+                                continue;
+                            Client client = data.get(i);
+                            if (!client.uploaded) {
+                                client.prior = 1 / client.getWaitingTime() + clientsNumber + client.getSizes();
+                                if (client.prior > maxPrior) {
+                                    maxPrior = client.prior;
+                                    clientWithMaxPrior = client;
+                                }
+                            }
+                        }
+                        //uploaduj plik z najlepszym priorytetem
+                        if (clientWithMaxPrior == null)
+                            continue;
+                        System.out.println(clientWithMaxPrior.getName());
+                        clientWithMaxPrior.uploaded = true;
+                        available[j] = false;
+                        uploading[j] = clientWithMaxPrior.getName();
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        disksWorker.start();
     }
 
-    public VBox generateDisk(String name) {
+    public VBox generateDisk(String name, String labelText) {
         VBox container = new VBox();
         container.alignmentProperty();
 
@@ -68,7 +130,7 @@ public class Main extends Application {
         circle.setStrokeWidth(2.0);
         circle.setStrokeType(StrokeType.INSIDE);
 
-        Label diskStatus = new Label("client2");
+        Label diskStatus = new Label(labelText);
         diskStatus.setTextFill(Color.rgb(147, 200, 203));
 
 
